de480af8cce42b27849f057a604789c06aa9f27e
Manoj Modhwadia <manojmodhwadia@outlook.com>
2019-03-26 14:21:54 +0000

    attempting to center map in current location

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,27 +1,66 @@
 import React, { Component } from "react";
-import ReactMapGL from "react-map-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-import { fromJS } from "immutable";
+import MapGL, { NavigationControl } from "react-map-gl";
 
-class Map extends Component {
-  state = {
-    viewport: {
-      width: 400,
-      height: 400,
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoom: 8
-    }
-  };
+const TOKEN = process.env.REACT_APP_MAPBOX_KEY;
+
+const navStyle = {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  padding: "10px"
+};
+
+export default class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 37.785164,
+        longitude: -100,
+        zoom: 2.8,
+        bearing: 0,
+        pitch: 0,
+        width: 500,
+        height: 500
+      }
+    };
+  }
+
+  componentWillMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      let longitude = position.coords.longitude;
+      let latitude = position.coords.latitude;
+    });
+    console.log("current location:", longitude, latitude);
+    this.setState({
+      longitude: longitude,
+      latitude: latitude
+    });
+  }
+
+  // _locateUser() {
+  //   // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
+  //   navigator.geolocation.getCurrentPosition(position => {
+  //     this.updateViewport({
+  //       longitude: position.coords.longitude,
+  //       latitude: position.coords.latitude
+  //     });
+  //   });
+  // }
 
   render() {
+    const { viewport } = this.state;
+
     return (
-      <ReactMapGL
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY}
-        {...this.state.viewport}
-        onViewportChange={viewport => this.setState({ viewport })}
-      />
+      <MapGL
+        {...viewport}
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapboxApiAccessToken={TOKEN}
+      >
+        <div className="nav" style={navStyle}>
+          <NavigationControl />
+        </div>
+      </MapGL>
     );
   }
 }
-export default Map;
